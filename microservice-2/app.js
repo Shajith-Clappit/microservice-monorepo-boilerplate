3f246beabfad5fd7cd911e2cd530e4d64e90a200
Shajith-Clappit <skafridi@clappit.io>
2025-01-21 12:23:54 +0530

    upd

---
+++ microservice-2/app.js
@@ -0,0 +1,13 @@
+console.log('This is Microservice 2!');
+const express = require('express');
+const app = express();
+
+app.get('/micro1', function (req, res) {
+  res.send('This is Microservice 1!');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
+
+//helloooo
